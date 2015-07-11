a3a4ea6970e956598b7fe0e73a9e0089d3543804
sarahsalder <sarahsalder@gmail.com>
2015-07-11 14:56:50 -0400

    storing to Chrome Storage

--- popup.js
+++ popup.js
@@ -1,26 +1,24 @@
 $(document).ready(function() {
+
     $('.listItemInput').focus(); //focus tell us what selector the code is reference
+    chrome.storage.sync.set({'item': []}, function() {} );
 
     $(document)
-
-    // yeah then you should be all set if you just use a regular JS array 
-    // and do a .get before every .save to see if you need to make it or 
-    // append
-
     // Add to list
     .on('click', '.addToList', function() {
-        var itemToAdd = $('input[name="listItemInput"]').val().trim(); //.val gets current value, trim maybe deletes white s
-        if ( itemToAdd ) { // as long as the variable is not null----- then itll workout (an assertion)
-            $('.listItems').append('<div class="input"><input type="checkbox" name="item" class="item"/> '+ itemToAdd +'</div>'); //value defines initial value before anything is entered
-            chrome.storage.sync.set({'item': itemToAdd}, function() {
-            // Notify that we saved.
-         });
-        }
-        $('.listItemInput').val('').focus(); //focuses on listItemInput and clears the value
-    })
-
-
+        var itemToAdd = $('input[name="listItemInput"]').val().trim(); 
+            $('.listItems').append('<div class="input"><input type="checkbox" name="item" class="item"/> '+ itemToAdd +'</div>'); 
+            var oldItems;
+            chrome.storage.sync.get(null, function(data) { 
+                oldItems = data["item"];
+                oldItems.push(itemToAdd);
+                chrome.storage.sync.set({'item': oldItems}, function() {});
+                $('.listItemInput').val('').focus(); //focuses on listItemInput and clears the value
+            });
+    });
+   
     
+    $(document)
     // Remove from list
     .on('change', '.item', function() { 
         if( $(this).is(':checked') ){
@@ -35,36 +33,5 @@ $(document).ready(function() {
         return false; //dont do normal submit... but now we have an event handler e.. 
     });
 
-
-      // function saveChanges() {
-      //   // Get a value saved in a form.
-      //   var theValue = textarea.value;
-      //   // Check that there's some code there.
-      //   if (!theValue) {
-      //     message('Error: No value specified');
-      //     return;
-      //   }
-      //   // Save it using the Chrome extension storage API.
-      //   chrome.storage.sync.set({'value': theValue}, function() {
-      //     // Notify that we saved.
-      //     message('Settings saved');
-      //   });
-      // }
-
-
-// When establishing a connection, each end is given a runtime.
-// Port object which is used for sending and receiving messages through 
-// that connection.
-// Here is how you open a channel from a content script, 
-// and send and listen for messages:
-
-// var port = chrome.runtime.connect({name: "knockknock"});
-// port.postMessage({joke: "Knock knock"});
-// port.onMessage.addListener(function(msg) {
-//   if (msg.question == "Who's there?")
-//     port.postMessage({answer: "Madame"});
-//   else if (msg.question == "Madame who?")
-//     port.postMessage({answer: "Madame... Bovary"});
-// });
 
 });

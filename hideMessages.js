0362dbfdd73be329391020c19a5380d30951cde1
sarahsalder <sarahsalder@gmail.com>
2015-07-16 17:11:18 -0400

    does everything but unhide on remove

--- hideMessages.js
+++ hideMessages.js
@@ -1,30 +1,27 @@
-var block = {};
+
 $(document).ready(function() {
 
 var unreadMessages = document.getElementsByClassName("unreadMessage");
 
+var previousEntries;
 
-function loadData(data) { 
-        block.previousEntries = data["item"];
+chrome.storage.sync.get(null, function(data){
+    previousEntries = (data["item"]);
+    killMessages();
+});
+
+function killMessages(){
+    for (var i=0; i < unreadMessages.length; i++) {
+    var messagePreview = unreadMessages[i].getElementsByClassName("previewline");
+
+    for (var j=0; j < previousEntries.length; j++) {
+      var messageString = $(messagePreview[0]).text(); 
+      if (messageString.includes(previousEntries[j])) {
+        $(unreadMessages[i]).hide();
+      }
     }
-
-chrome.storage.sync.get(null, loadData);
-
-
-console.log(block.previousEntries);
-
-
-  // for (var i=0; i < unreadMessages.length; i++) {
-  //   var messagePreview = unreadMessages[i].getElementsByClassName("previewline");
-
-  //   for (var j=0; j < previousEntries.length; j++) {
-  //     var messageString = $(messagePreview[0]).text(); 
-  //     if (messageString.includes(previousEntries[j])) {
-  //       $(unreadMessages[i]).hide();
-  //     }
-  //   }
-  // }
-
+  }
+}
 
 
 });
